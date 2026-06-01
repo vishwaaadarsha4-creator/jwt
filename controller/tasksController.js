4d7f29d283b8769850b4b4a9a2feac8ecf0be224
vishwaaadarsha4-creator <vishwaaadarsha4@gmail.com>
2026-06-01 10:17:21 +0545

    create task page

--- controller/tasksController.js
+++ controller/tasksController.js
@@ -1,3 +1,7 @@
 exports.getAllTasks = (req,res)=>{
     res.render("allTaks");
 }
+
+exports.getCreateTask = (req,res)=>{
+    res.render("createTask");
+}
--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -1,8 +1,9 @@
-const { getAllTasks } = require("../controller/tasksController");
+const { getAllTasks, getCreateTask } = require("../controller/tasksController");
 
 const router = require("express").Router();
 
 router.route("/alltasks").get(getAllTasks);
+router.route("/createtask").get(getCreateTask);
 
 
 module.exports = router;
